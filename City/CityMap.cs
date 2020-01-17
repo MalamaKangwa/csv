8b50c4a54a18d00cf7cc3d23f016b54e371e25d2
MalamaKangwa <MalamaKangwa@szi.gov.zm>
2020-01-17 15:46:22 +0200

    Task: Refactor 3 - Seperated Cities Context, CityEntities and Country Entity

--- City/CityMap.cs
+++ City/CityMap.cs
@@ -1,4 +1,5 @@
 ﻿using CsvHelper.Configuration;
+using Model;
 
 namespace Cities
 {
---
+++ csv/Writecsv.cs
@@ -0,0 +1,29 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Model;
+
+namespace Csv
+{
+        public class Writecsv
+        {
+            public static string WriteCsvFile(IOrderedEnumerable<CityModel> QueryX)
+            {
+                var queryName = nameof(QueryX);
+                var fileAddress = "c://csvWriteFiles//" + queryName + ".csv";
+
+                using (var writer = new StreamWriter(fileAddress))
+                using (var csv = new CsvWriter(writer))
+                {
+                    csv.WriteRecords(QueryX);
+                }
+
+                return fileAddress;
+            }
+        }
+    
+}
+
